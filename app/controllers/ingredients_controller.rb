e679e9076d7a046e71f54fff92c973e83948ac34
hiromiromi <rmhnhns1225@yahoo.ne.jp>
2025-01-22 13:22:19 +0900

    食材表示

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,6 +2,7 @@ class IngredientsController < ApplicationController
   
   def index
     @nutrient = Nutrient.find(params[:id])
+    @ingredients = Ingredient.where(nutrient_id: params[:id])
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2025_01_17_053511) do
   create_table "ingredients", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "ingredient_nutrient_id", null: false
+    t.integer "nutrient_id", null: false
     t.text "detail", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
