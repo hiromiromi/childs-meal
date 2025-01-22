7f6a06c3f91be608a76b683c354b79f05976d41e
hiromiromi <rmhnhns1225@yahoo.ne.jp>
2025-01-22 17:12:44 +0900

    レシピview記述（途中）

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
     resources :nutrients, only: [:index ]
       get 'nutrients/:id', to: 'ingredients#index', as: 'ingredient'
       get 'ingredients/:id', to: 'ingredients#show', as: 'ingredient_detail'
+      get 'recipes/:id', to: 'recipes#show', as: 'recipe_show'
 end 
