b6a0f8bb04453cf35f65073928938503d6c7c634
hiromiromi <rmhnhns1225@yahoo.ne.jp>
2025-01-23 12:59:35 +0900

    レシピ表示機能（途中まで）

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -7,6 +7,7 @@ class IngredientsController < ApplicationController
 
   def show
     @ingredient = Ingredient.find(params[:id])
+    @recipes = Recipe.all
   end
 
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,4 @@
 class Recipe < ApplicationRecord
+
+  validates :dish_name, :material, :dish_recipe, :recipe_nutrients, presence: true
 end
