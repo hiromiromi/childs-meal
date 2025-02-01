27529c87a033c807685f09f72fb3dccebf44018e
hiromiromi <rmhnhns1225@yahoo.ne.jp>
2025-02-01 23:50:36 +0900

    お気に入り登録実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,28 +3,37 @@ class FavoritesController < ApplicationController
   before_action :set_recipe
   before_action :set_column
 
-def create
-  @favorites_recipe = Favorite.create(user_id: current_user.id, recipe_id: @recipe.id)
-  @favorites_column = Favorite.create(user_id: current_user.id, column_id: @column.id)
-  
-end
+  def new
+    @favorite = Favorite.new
+  end
 
-def destroy
-  @favorite_recipe = Favorite.find_by(user_id: current_user.id, recipe_id: @recipe.id)
-  @favorite_recipe.destroy
+  def create
+    @favorites_recipe = Favorite.create(user_id: current_user.id, recipe_id: @recipe.id)
+      respond_to do |format|
+        format.js
+      end
+    @favorites_column = Favorite.create(user_id: current_user.id, column_id: @column.id)
+  end
 
-  @favorite_column = Favorite.find_by(user_id: current_user.id, column_id: @column.id)
-  @favorite_column.destroy
-end
+  def destroy
+    @favorite_recipe = Favorite.find_by(user_id: current_user.id, recipe_id: @recipe.id)
+    @favorite.destroy if @favorite
+    respond_to do |format|
+      format.js # d
+    end
+
+    @favorite_column = Favorite.find_by(user_id: current_user.id, column_id: @column.id)
+    @favorite_column.destroy
+  end
 
 
 private
-def set_recipe
-  @recipe = Recipe.find(params[:recipe_id])
-end
+  def set_recipe
+    @recipe = Recipe.find(params[:recipe_id])
+  end
 
-def set_column
-  @column = Column.find(params[:column_id])
-end
+  def set_column
+    @column = Column.find(params[:column_id])
+  end
 
 end
